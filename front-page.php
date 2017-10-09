8ca5440a9f8448cf504dd97b8e0d1a0af8c11c35
mariusjopen <mail@mariusjopen.com>
2017-10-09 20:30:09 +0200

    basic work template setup

--- front-page.php
+++ front-page.php
@@ -1,7 +1,27 @@
 <?php get_header(); ?>
 
+<div class="wrapper">
 
-<p>FRONT PAGE</p>
+  <div class="work">
+    <?php
+    query_posts(array(
+        'post_type' => 'work',
+    ) );
 
+    while (have_posts()) : the_post();
+    ?>
+      <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+      <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+      <p><?php echo get_the_excerpt(); ?></p>
+    <?php
+    endwhile;
+    ?>
+  </div>
+
+  <div class="news">
+
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
